0f0561177d4504cb9502b872630a2680ed8352cc
Vexirale <vexirale@gmail.com>
2025-07-18 02:21:02 +0200

    Prepare for GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/fatbikeparts.eu/", // <-- 👈 ADD THIS LINE
+  base: "https://fatbikeparts.eu/", // <-- 👈 ADD THIS LINE
   server: {
     host: "::",
     port: 8080,
